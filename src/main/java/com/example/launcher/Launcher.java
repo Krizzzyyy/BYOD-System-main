58971da49fefd4a1b91830c413c46590286298cc
JohnErich04 <jerichfamilaran04@gmail.com>
2026-07-02 10:35:35 +0800

    word_fixes

--- src/main/java/com/example/launcher/Launcher.java
+++ src/main/java/com/example/launcher/Launcher.java
@@ -3,7 +3,7 @@ package com.example.launcher;
 import com.example.login.LoginApp;
 import javafx.application.Application;
 
-public class    Launcher {
+public class Launcher {
     public static void main(String[] args) {
         Application.launch(LoginApp.class, args);
     }
